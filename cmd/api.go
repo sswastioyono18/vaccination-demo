37e66acc05336edafde3f0d251195a5e0488d4b3
tio <email.tyo@gmail.com>
2021-10-16 20:27:59 +0700

    changed to 0.0.0.0

--- cmd/api.go
+++ cmd/api.go
@@ -64,7 +64,7 @@ func main() {
 	router := ResidentRoutes(newResidentService)
 	srv := &http.Server{
 		Handler: router,
-		Addr:    "127.0.0.1:8000",
+		Addr:    "0.0.0.0:8000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
